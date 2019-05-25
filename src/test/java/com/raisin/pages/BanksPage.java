41583978343e840a541c377ad18ed76a06a44c6d
Nicolas Mori <nicolasmori@Nicolass-iMac.local>
2019-05-25 17:05:24 +0200

    d

--- src/test/java/com/raisin/pages/BanksPage.java
+++ src/test/java/com/raisin/pages/BanksPage.java
@@ -2,8 +2,6 @@ package com.raisin.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 import com.raisin.framework.ParentPage;
 
@@ -17,9 +15,8 @@ public class BanksPage extends ParentPage {
 	// Locator section
 	By menuOptionsTab = By.xpath("//*[@id='all-tab']");
 	By menuOptionProducts = By.xpath("//*[contains(@href,'Products')]");
-	
-	String buttonDetailsWithParametersOfBankName = "//div[contains(@class,'innerContainer')]/div/img[contains(@src,'%s')]/parent::*/parent::*/following-sibling::div";
-	
+	By optionProduct = By.xpath("//*[@id='offer-FIM003']/div[2]/div/div/button/span");
+
 	// Method section
 	/**
 	 * this method make the change the selector of flight type to one way
@@ -27,12 +24,8 @@ public class BanksPage extends ParentPage {
 	 */
 	public void selectTheBackUsingTheParameters(String bankCodeName) {
 		System.out.println("Starting to send some value of the username");
-		clickJSx2(menuOptionProducts);		
-		
-		waitForAnExplicitElement(menuOptionsTab);
-		Actions action = new Actions(driver);
-		WebElement we = driver.findElement(menuOptionsTab);
-		action.moveToElement(we).moveToElement(driver.findElement(By.xpath("//*[@id='offer-FIM003']/div[2]/div/div/button/span"))).click().build().perform();
-		
+		driver.findElement(menuOptionProducts);
+		clickJSx2(menuOptionProducts);
+		mouseOverAndClick(menuOptionsTab, optionProduct);
 	}
 }
--- src/test/java/com/raisin/steps/StepsHelper.java
+++ src/test/java/com/raisin/steps/StepsHelper.java
@@ -1,9 +1,5 @@
 package com.raisin.steps;
 
-
-
 public class StepsHelper {
 	// this class provide help to share data between the different steps classes.
-
-	
 }
